151afe894ee9ef6fcd0035006309625e6a3e4aed
thierno sadou diallo <thierved01@gmail.com>
2019-06-07 18:02:23 +0000

    problem solved

--- day1.py
+++ day1.py
@@ -7,7 +7,7 @@
 def middle_node(head, node_list):
     slow = head
     fast = head
-    while (fast.next and fast.next.next):
+    while (fast and fast.next):
         slow = node_list.next
         fast = node_list.next.next
     return slow
