54ae97b00a461e92ef02a012277bf95cf0964d12
thierno sadou diallo <thierved01@gmail.com>
2019-06-11 17:48:49 +0000

    solved!

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-from day3 import sortArrayByParity
-
+from day4 import numJewelsInStones
 
 if __name__ == "__main__":
-   print('===')
-   result = sortArrayByParity([3,1,2,4])
+   result = numJewelsInStones("aA", "aAAbbbb")
    print(result)
